86cdaaf0e15444df1e49b764cf3df97b64762f61
Daniel Noh <dnoh@itmgroup.mx>
2019-07-30 10:59:11 -0500

    Fixed active of main menu on Destinations Module

--- core/app/controllers/Destinations.php
+++ core/app/controllers/Destinations.php
@@ -18,6 +18,8 @@ class Destinations extends CI_Controller
 
         $this->load->Model('Page');
         $this->Page->page_name = $view;
+        $this->Page->menu_active = $view;
+        $this->Page->submenu_active = $option;
 
         $data = $this->Page->get_contents();
 
