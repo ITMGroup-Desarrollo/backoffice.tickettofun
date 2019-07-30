a61e2580e10357f6d3d472c47a8fdd19c7bd0378
Jorge Saurina <jpadron@itmgroup.mx>
2019-07-30 09:26:49 -0500

    Upload Destination Module.

---
+++ core/app/controllers/Destinations.php
@@ -0,0 +1,94 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Destinations extends CI_Controller
+{
+    /**
+    *Index page for this controller
+    */
+    public function index()
+    {
+        $this->load->library('user_session', NULL, 'user');
+
+        if ( ! $this->user->active_session())
+            redirect(base_url('signin'));
+
+        $view   = $this->uri->segment(1);
+        $option = $this->uri->segment(2);
+
+        $this->load->Model('Page');
+        $this->Page->page_name = $view;
+
+        $data = $this->Page->get_contents();
+
+        $this->load->Model('Destination');
+
+        if ($option == 'list') 
+        {
+            $table = $this->Destination->get_list();
+
+            $data['contents'] = str_replace(
+                '{title}', 'List of destinations', $data['contents']
+            );
+
+            $data['contents'] = str_replace(
+                '{content}', $table, $data['contents']
+            );
+        }
+        else
+        {
+            $form = $this->Destination->get_form();
+            $form = str_replace('{id}', 'add-destination', $form);
+
+            $data['contents'] = str_replace(
+                '{title}', 'New destination', $data['contents']
+            );
+
+            $data['contents'] = str_replace(
+                '{content}', $form, $data['contents']
+            );
+        }
+
+        $this->load->view('Master', $data);
+    }
+
+    /**
+    *Update page for this controller
+    */
+    public function update()
+    {
+        $this->load->library('user_session', NULL, 'user');
+
+        if ( ! $this->user->active_session())
+            redirect(base_url('signin'));
+
+        $view   = $this->uri->segment(1);
+        $option = $this->uri->segment(2);
+
+        $this->load->Model('Page');
+        $this->Page->page_name = $view;
+
+        $data = $this->Page->get_contents();
+
+        $this->load->Model('Destination');
+
+        $form = $this->Destination->get_form();
+        $form = str_replace('{id}', 'update-destination', $form);
+
+        $data['contents'] = str_replace(
+            '{title}', 'Edit destination', $data['contents']
+        );
+
+        $data['contents'] = str_replace(
+            '{content}', $form, $data['contents']
+        );
+
+        $destination = $this->Destination->get_data($option);
+        $destination = 'window.destination = ' . json_encode($destination);
+
+        $script = custom('script', '', $destination);
+        $data['scripts'] = $script .  $data['scripts'];
+
+        $this->load->view('Master', $data);
+    }
+}
---
+++ core/app/models/Destination.php
@@ -0,0 +1,146 @@
+<?php
+
+/**
+* Client Class
+*
+* @package CodeIgniter
+* @category Models
+* @Author ITM Dev Team
+* @Since Version 1.0.0
+*/
+class Destination extends CI_Model
+{
+    public $model;
+    public $active;
+    public $attrib;
+    public $inactive;
+    public $anchor_attrib;
+
+    public function __construct()
+    {
+        $this->model = '';
+        parent::__construct();
+
+        $this->attrib = array('class' => 'center');
+        $this->active = array('class' => 'label label-success');
+        $this->inactive = array('class' => 'label label-danger');
+    }
+
+    public function get_list()
+    {
+        $this->db->close();
+        $table_content = $this->Page->get_settings('destinations');
+        $table_content = $this->build->build_components(
+            $table_content['DESTINATIONS_TABLE']
+        );
+
+        // Call API here!
+        $params = new stdClass();
+        $endpoint = HOST . GET_DESTINATIONS_ROUTE;
+
+        $this->load->library('session');
+        $token = $this->session->userdata('token');
+
+        $response = json_decode(
+            $this->api->request_api('GET', $endpoint, $params, $token)
+        );
+
+        if ($response->code == 200)
+        {
+            $rows = $response->message;
+
+            foreach ($rows as $row)
+            {
+                $aux = '';
+                $this->anchor_attrib = array();
+
+                $aux .= custom('td', '', $row->destination_name);
+                $aux .= custom('td', '', $row->country);
+
+                $status = '';
+                $delete = '';
+                if ($row->active_status == 1)
+                {
+                    $status = custom('span', $this->active, 'Active');
+                }
+                else
+                {
+                    $status = custom('span', $this->inactive, 'Inactive');
+                }
+
+                $status_attrib = $this->attrib;
+                $status_attrib['data-status'] =  $row->destination_id;
+                $aux .= custom('td', $status_attrib, $status);
+
+                $path = 'destinations/' . $row->destination_id;
+                $this->anchor_attrib['class'] = 'edit';
+                $this->anchor_attrib['href'] = base_url($path);
+                $edit = custom('i', array('class' => 'ion-edit'), '');
+
+                $edit = custom('a', $this->anchor_attrib, $edit);
+
+                if ($row->active_status == 1) {
+                    $this->anchor_attrib['href'] = '#';
+                    $this->anchor_attrib['class'] = 'delete';
+                    $this->anchor_attrib['data-id'] = $row->destination_id;
+                    $delete = custom('i', array('class' => 'ion-close'), '');
+
+                    $delete = custom('a', $this->anchor_attrib, $delete);
+                }
+
+                $aux .= custom('td', $this->attrib, $edit . $delete);
+
+                $this->model .= custom('tr', '', $aux);
+            }
+        }
+        else
+        {
+            $aux = '';
+            for ($i = 0; $i < 4; $i++)
+                $aux .= custom('td', '', '');
+
+            $this->model = custom('tr', '', $aux);
+        }
+
+        $this->model = str_replace('{rows}', $this->model, $table_content);
+
+        return $this->model;
+    }
+
+    public function get_form()
+    {
+        $this->db->close();
+        $contents = $this->Page->get_settings('destinations');
+
+        $this->model = $this->build->build_components(
+            $contents['DESTINATIONS_FORM']
+        );
+
+        return $this->model;
+    }
+
+    public function get_data($id)
+    {
+        $endpoint = HOST . GET_DESTINATIONS_ROUTE . '/' . $id;
+
+        $params = new stdClass();
+        $this->load->library('session');
+        $token = $this->session->userdata('token');
+
+        $response = json_decode(
+            $this->api->request_api('GET', $endpoint, $params, $token)
+        );
+
+        $destination = new stdClass();
+
+        if ($response->code == 200)
+        {
+            $destination->id       = $response->message->destination_id;
+            $destination->name     = $response->message->destination_name;
+            $destination->country  = $response->message->country;
+            $destination->active   = $response->message->active_status;
+        }
+
+        return $destination;
+    }
+}
