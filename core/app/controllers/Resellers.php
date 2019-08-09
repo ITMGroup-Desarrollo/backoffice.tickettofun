1d0cb8d16d683f9afbab8492f5cb7185255519c1
Martin <Ucan>
2019-08-09 15:12:59 -0500

    Apply solution of issue #13

--- core/app/controllers/Resellers.php
+++ core/app/controllers/Resellers.php
@@ -18,6 +18,8 @@ class Resellers extends CI_Controller
 
         $this->load->Model('Page');
         $this->Page->page_name = $view;
+        $this->Page->menu_active = 'vendors';
+        $this->Page->submenu_active = $option;
 
         $data = $this->Page->get_contents();
 
@@ -67,8 +69,6 @@ class Resellers extends CI_Controller
 
         $this->load->Model('Page');
         $this->Page->page_name = $view;
-        $this->Page->menu_active = 'vendors';
-        $this->Page->submenu_active = $option;
 
         $data = $this->Page->get_contents();
 
