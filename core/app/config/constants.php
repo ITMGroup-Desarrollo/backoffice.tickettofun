067e473a3c85ce9c86e7a9f639f9e2900a1d9c6b
Daniel Noh <dnoh@itmgroup.mx>
2019-12-02 06:42:44 -0500

    Fix host url

--- core/app/config/constants.php
+++ core/app/config/constants.php
@@ -92,7 +92,7 @@ defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest auto
 | Used to getting token access to core.
 |
 */
-defined('HOST') OR define ('HOST', 'https://core.itmgroup.mx');
+defined('HOST') OR define ('HOST', 'https://core.itmgroup.mx/');
 defined('TOKEN_ROUTE') OR define('TOKEN_ROUTE', 'v1/token');
 defined('GET_SERVICES_ROUTE') OR define('GET_SERVICES_ROUTE', 'api/v1/products');
 defined('GET_LOCATIONS_ROUTE') OR define('GET_LOCATIONS_ROUTE', 'api/v1/locations');
