54359532cb44b11c267ff1a8fea7a71f00bbb637
Martin <Ucan>
2020-01-16 17:27:33 -0500

    Remove value 0 in the column actions

--- js/vendor/config_base.js
+++ js/vendor/config_base.js
@@ -117,7 +117,7 @@ var config = {
               return `<a class="btn-link edit" data-toggle="tooltip" data-placement="left" title="Edit allotment" href="configuration/${row[12]}"><i class="fas fa-edit"></i></a> <a class="btn-link delete" data-toggle="tooltip" data-placement="left" title="Delete allotment" data-id="${row[12]}"><i class="fas fa-trash"></i></a>`
             }
           } else {
-            return ''+row[13];
+            return '';
           }
         }
       })
