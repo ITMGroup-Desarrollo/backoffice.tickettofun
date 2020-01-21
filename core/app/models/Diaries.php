c5978dc54d1d8c3ddd09556a0240ceb631495efb
Wendy <egalvan@itmgroup.mx>
2020-01-21 12:24:15 -0500

    Fix paint private service on diary module

--- core/app/models/Diaries.php
+++ core/app/models/Diaries.php
@@ -103,7 +103,7 @@ class Diaries extends CI_Model
                 $aux .= custom('td', '', $row->schedule_start);
                 $aux .= custom('td', '', $row->schedule_end);
                 $aux .= custom('td', '', $row->duration);
-                if ($row->private_service === 1){
+                if ($row->private_service == 1){
                     $aux .= custom('td', '', 'Yes');
                 }else{
                     $aux .= custom('td', '', 'No');
