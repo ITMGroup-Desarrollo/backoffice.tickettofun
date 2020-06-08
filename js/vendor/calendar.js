aea68dfbc3cd2c5b2372e4137e0a61073ab3bd9d
Daniel Noh <dnoh@itmgroup.mx>
2020-06-08 15:50:48 -0500

    Update calendar
    Plugin version, styles

--- js/vendor/calendar.js
+++ js/vendor/calendar.js
@@ -6,17 +6,23 @@ for (var i = 0, l = events.length; i < l; i++) {
   events[i].start = new Date(events[i].start)
 }
 
-$(document).ready(function () {
-  $('#full-calendar').fullCalendar({
+document.addEventListener('DOMContentLoaded', function () {
+  var element = document.getElementById('full-calendar')
+
+  var calendar = new FullCalendar.Calendar(element, {
+    plugins: ['interaction', 'dayGrid', 'timeGrid', 'list'],
+    height: 'parent',
     header: {
-      left: 'month,agendaWeek,agendaDay',
+      left: 'prevYear,prev,next,nextYear today',
       center: 'title',
-      right: 'today prev,next'
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
     },
-    selectable: false,
-    selectHelper: false,
-    editable: false,
-    events: events,
-    eventBackgroundColor: '#278ccf'
+    defaultView: 'dayGridMonth',
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    eventLimit: true,
+    events: events
   })
+
+  calendar.render()
 })
