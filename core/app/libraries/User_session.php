b88934c631e79915eb47c93e43660b28d02a8d29
Daniel Noh <dnoh@itmgroup.mx>
2020-08-20 10:57:32 -0500

    Add function to get action buttons accord about permissions assigned

--- core/app/libraries/User_session.php
+++ core/app/libraries/User_session.php
@@ -71,12 +71,104 @@ class User_session
         return $active;
     }
 
+    public function get_actions($table) {
+        return $this->_get_actions_elements($table);
+    }
+
     private function _get_permissions($data_permissions) {
 
         $permissions = array();
 
         for ($i = 0; $i < count($data_permissions); $i++) {
             $permissions[$i] = $data_permissions[$i]->menu_name;
+        }
+
+        return $permissions;
+    }
+
+    /**
+    * Validate permissions on table
+    *
+    * @param string table name
+    * @return array permissions on table an actions buttons
+    */
+    private function _get_actions_elements($table) {
+        $this->CI->load->library('session');
+
+        $anchor_attrib = array();
+
+        $permissions = array(
+            'g' => 0,
+            'gElement' => '',
+            'i' => 0,
+            'iElement' => '',
+            'u' => 0,
+            'uElement' => '',
+            'd' => 0,
+            'dElement' => '',
+            'statusElement' => ''
+        );
+
+        $status_attrib = array(
+            'class' => 'badge badge-{status}',
+            'data-status' => '{status_value}'
+        );
+
+        $permissions['statusElement'] = custom('span', $status_attrib, '{s_text}');
+
+        $rol = $this->CI->session->userdata('rol_id');
+
+        $get = "g_{table}";
+        $insert = "i_{table}";
+        $update = "u_{table}";
+        $delete = "d_{table}";
+
+        if ($rol == 1 || in_array($get, $this->CI->session->userdata('permissions'))) {
+            $permissions['g'] = 1;
+        }
+
+        if ($rol == 1 || in_array($insert, $this->CI->session->userdata('permissions'))) {
+            $permissions['i'] = 1;
+
+            if ($table == 'arrives') {
+                $anchor_attrib['class'] = 'schedule';
+                $anchor_attrib['href'] = base_url() . 'allotments/itinerary/{id}';
+
+                $itinerary = custom('i', array('class' => 'fas fa-calendar-alt'), '');
+                $itinerary = custom('a', $anchor_attrib, $itinerary);
+
+                $anchor_attrib['class'] = 'clone';
+                $anchor_attrib['href'] = base_url() . 'allotments/clone/{id}';
+
+                $clone = custom('i', array('class' => 'fas fa-clone'), '');
+                $clone = custom('a', $anchor_attrib, $clone);
+
+                $permissions['iElement'] = $clone . $itinerary;
+            }
+        }
+
+        if ($rol == 1 || in_array($update, $this->CI->session->userdata('permissions'))) {
+            $permissions['u'] = 1;
+            $anchor_attrib['class'] = 'edit';
+            $anchor_attrib['href'] = base_url() . 'arrives/{id}';
+
+            $edit = custom('i', array('class' => 'fas fa-edit'), '');
+            $edit = custom('a', $anchor_attrib, $edit);
+
+            $permissions['uElement'] = $edit;
+        }
+
+        if ($rol == 1 || in_array($delete, $this->CI->session->userdata('permissions'))) {
+            $permissions['d'] = 1;
+
+            $anchor_attrib['href'] = '#';
+            $anchor_attrib['class'] = 'delete';
+            $anchor_attrib['data-id'] = '{id}';
+
+            $delete = custom('i', array('class' => 'fas fa-trash'), '');
+            $delete = custom('a', $anchor_attrib, $delete);
+
+            $permissions['dElement'] = $delete;
         }
 
         return $permissions;
